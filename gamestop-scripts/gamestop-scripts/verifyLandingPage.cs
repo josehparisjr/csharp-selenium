ac8d1b5b30dc0fa65fbbe4e958592cef2bd602b4
josehparisjr <jose.hparisjr@gmail.com>
2018-10-27 15:26:08 -0400

    gamestop-scripts (setup with sample test) - Nico

---
+++ gamestop-scripts/gamestop-scripts/verifyLandingPage.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace gamestop_scripts
+{
+    [TestFixture]
+    public class verifyLandingPage
+    {
+
+        [Test]
+        public void siteHeaderIsDisplayed()
+        {
+            IWebDriver browser = new ChromeDriver();
+            browser.Navigate().GoToUrl("http://gamestop.com");
+            IWebElement header = browser.FindElement(By.Id("Img1"));
+    
+            Assert.True(header.Displayed);
+            browser.Close();
+        }
+    }
+}
